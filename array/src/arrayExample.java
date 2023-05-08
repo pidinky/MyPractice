8448b83a73240b600ab1d99dd848a836b1d49212
pidinky <pidinky177@gmail.com>
2023-05-09 00:12:44 +0200

    Add some restrictions for input.
    Add a loop so the user can input the month again in case the number
    is out of range.
    Also add restriction in case the user input negative number.

--- array/src/arrayExample.java
+++ array/src/arrayExample.java
@@ -4,71 +4,74 @@ public class arrayExample {
     public static void main(String[] args) {
 
         int daysOfMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int month;
-        String monthName;
+        boolean isValidInput = false;
 
         Scanner sc = new Scanner(System.in);
 
-
+        int month;
         System.out.println("Input the month: ");
         if(!sc.hasNextInt()){
             System.out.println("Invalid input");
             sc.close();
             return;
         }
-        month = sc.nextInt();
-        if (month > 12) {
-            System.out.println("Invalid month");
+
+        while (!isValidInput) {
+            month = sc.nextInt();
+            if (month > 12 || month < 1) {
+                System.out.println("Invalid month. Try again...");
+                continue;
+            }
+            isValidInput = true;
+
+            String monthName;
+            switch (month) {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+                    break;
+                default:
+                    monthName = "Invalid month.";
+                    break;
+            }
+
+            System.out.print("You input month " + monthName + ". ");
+            System.out.println("Your month has " + daysOfMonth[month - 1] + " days.");
+
             sc.close();
-            return;
         }
-
-        switch (month) {
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-                break;
-            default:
-                monthName = "Invalid month.";
-                break;
-        }
-
-        System.out.print("You input month " + monthName + ". ");
-        System.out.println("Your month has " + daysOfMonth[month - 1] + " days.");
-
-        sc.close();
-
     }
 }
+
