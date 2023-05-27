4e0c05818ac3c66a57067e6f18efd5c341b3a60a
pidinky <pidinky177@gmail.com>
2023-05-27 11:16:54 +0200

    Small changes in 'matrix' project

--- matrix/src/Matrix.java
+++ matrix/src/Matrix.java
@@ -1,23 +1,38 @@
 import java.util.Scanner;
 
 public class Matrix {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int mat[][] = new int[3][3];
+        int n;
+        System.out.println("Input the dimension of square matrix:");
+        if(!sc.hasNextInt()) {
+            System.out.println("Invalid input.");
+            sc.close();
+            return;
+        }
+        n = sc.nextInt();
 
+        int mat[][] = new int[n][n];
         System.out.println("Input matrix values:");
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                mat[i][j] = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (!sc.hasNextInt()){
+                    System.out.println("Invalid input");
+                    sc.close();
+                    return;
+                }
+                    mat[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+
+        System.out.println("Your matrix:");
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 System.out.print(mat[i][j] + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
+        sc.close();
     }
 }
