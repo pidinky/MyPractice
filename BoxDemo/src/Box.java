55fe6e8379fbf94a03e5fb2091f4502330cf88cb
pidinky <pidinky177@gmail.com>
2023-06-05 12:04:30 +0200

    Add changes to BoxDemo Trying to practice constructor overload.

--- BoxDemo/src/Box.java
+++ BoxDemo/src/Box.java
@@ -3,14 +3,30 @@ public class Box {
     double width;
     double length;
 
+    public Box(){
+        height = 0;
+        width = 0;
+        length = 0;
+    }
+
+    public Box(double i) {
+        height = width = length = i;
+    }
+
+    public Box(double height, double width, double length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
     double volume(){
 
         return height * width * length;
     }
 
-    void setDimensions(double h, double w, double l){
+    /*void setDimensions(double h, double w, double l){
         height = h;
         width = w;
         length = l;
-    }
+    }*/
 }
